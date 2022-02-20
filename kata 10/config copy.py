40720d5293f6e4e937e3d420055d0110c3acf796
DilianUs <75262812+DilianUs@users.noreply.github.com>
2022-02-19 20:45:43 -0600

    Add files via upload

---
+++ kata 10/config copy.py
@@ -0,0 +1,28 @@
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+
+main()
+
+if __name__ == '__main__':
+    main()
+    
+def main():
+    try:
+        configuration = open('config.txt')
+    except Exception:
+        print("Couldn't find the config.txt file!")    
+
+main()
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+
+main()
